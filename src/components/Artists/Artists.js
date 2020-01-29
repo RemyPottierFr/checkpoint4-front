b5cd59c1aa9a5d30c66a1d485fe01729158a9cc8
Rémy <Remypottierpro@gmail.com>
2020-01-29 15:55:08 +0100

    Finish Artists Part with carrousel

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Artists.css';
 import Slider from "react-slick";
+import axios from 'axios';
 
 export default function Artists() {
+  const [artists, setArtists] = useState(null);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -10,19 +13,32 @@ export default function Artists() {
     slidesToShow: 1,
     slidesToScroll: 1
   };
+
+  useEffect(() => {
+    axios.get("/api/v1/artists")
+      .then(res => setArtists(res.data))
+  }, [])
+
   return (
     <div className="Artists">
       <p className="Artists__title">Our company</p>
       <Slider {...settings} className="Artists__slider">
-        <div>
-          <img src="/images/elChovito.jpg" className="Artists__slider-image" />
-          <div className="Artists__slider--desc">
-            <p></p>
-          </div>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
+        {
+          artists && artists.map((artist => (
+            <div className="Artists__slide" key={artist.name}>
+              <img src={`/images/artists/${artist.image}.jpg`} className="Artists__slider-image" alt="Portait of an artist" />
+              <div className="Artists__slider--desc">
+                <p className="Artists__slider--desc-title">{artist.name}, {artist.age} yo</p>
+                <div className="Slider__desc-type">
+                  <img src={`/icons/${artist.type}.png`} className="Desc__type-icon" alt="Icon of artist" />
+                  <span className="Desc__type-text">It's a/an <span>{artist.type}</span></span>
+                </div>
+                <p className="Slider__desc--text">{artist.description}</p>
+              </div>
+            </div>
+          )
+          ))
+        }
       </Slider>
     </div>
   )
